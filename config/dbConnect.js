5c583efe2181b4450ce8fd366d433f0e0dca25bb
birender <birenderk935@gmail.com>
2025-12-04 12:23:56 +0530

    db

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+mongoose.set('strictQuery',false);
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL,{useUnifiedTopology: true,useNewUrlParser: true,});
     console.log("DB Connected Successfully");
   } catch (error) {
     console.log("DB Connection failed:", error.message);
