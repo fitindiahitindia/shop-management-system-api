b0eec0d4b7433230e19d583adc0eca939930fbbe
birender <birenderk935@gmail.com>
2025-12-04 12:18:54 +0530

    db confige

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.LOCAL_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("DB Connected Successfully");
   } catch (error) {
     console.log("DB Connection failed:", error.message);
